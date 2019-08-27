d35c9e8d0639b1d2a7100269f347e836dc32d7de
chchstring <54307522+chchstring@users.noreply.github.com>
2019-08-27 15:07:52 +0800

    Add files via upload
    实体类

---
+++ Article.java
@@ -0,0 +1,64 @@
+package com.bit.blog.entity;
+
+import java.util.Date;
+
+public class Article {
+
+
+    private Integer id;
+    private String title;
+    private String content;
+    private Integer userId;
+    private Date create_time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", userId=" + userId +
+                ", createTime=" + create_time +
+                '}';
+    }
+}
